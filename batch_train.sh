53f3e1bee0c2b563b4109527211f6ccf41a2c319
CVT <constantin.tenzer@ens.psl.eu>
2025-03-18 20:14:06 +0100

    added batch

---
+++ batch_train.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=logs/test.out
+#SBATCH --error=logs/test.err
+#SBATCH --array=1-1:1
+#SBATCH --mem-per-cpu=5000
+#SBATCH --mail-type=ALL
+
+# Author : Constantin Vaillant-Tenzer
+# To see all the options, type sbatch --help
+# Usage sbatch batch_example.sh
+
+cd /home/adminialab #Replace with the relevant path
+
+source ~/.bashrc
+conda activate nodes_env
+
+cd /home/adminialab/GitFiles/nodes_bayesian #Replace with the relevant path
+
+# Creates the logs directory if it does not exist
+if [ ! -d logs ]; then
+    mkdir logs
+fi
+
+python main.py
+python main_vanila.py
+
+conda deactivate
